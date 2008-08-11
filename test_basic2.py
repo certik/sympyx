b531bde3a8e45b553dc2995f159736ad0cf6d2ec
Ondrej Certik <ondrej@certik.cz>
2008-08-11 22:35:11 +0200

    tests added

---
+++ test_basic2.py
@@ -0,0 +1,70 @@
+from csympy import Symbol, Integer
+
+import py
+
+x = Symbol("x")
+y = Symbol("y")
+z = Symbol("z")
+
+# basic sympy objects
+basic_objs = [
+    Integer(2),
+    x,
+    y,
+    pow(x,y)*y,
+]
+
+# all supported objects
+all_objs = basic_objs + [
+    5,
+]
+
+def dotest(s):
+    for x in all_objs:
+        for y in all_objs:
+            s(x,y)
+
+def test_basic():
+    def s(a,b):
+        x = a
+        x = +a
+        x = -a
+        x = a+b
+        x = a-b
+        x = a*b
+        x = a/b
+        x = a**b
+    dotest(s)
+
+def test_ibasic():
+    def s(a,b):
+        x = a
+        x += b
+        x = a
+        x -= b
+        x = a
+        x *= b
+        x = a
+        x /= b
+    dotest(s)
+
+def test_basic_nostr():
+    for obj in basic_objs:
+        for op in ['+','-','*','/','**']:
+            py.test.raises(TypeError, "obj %s '1'" % op)
+
+def test_len():
+    e = x*y
+    assert len(e.args) == 2
+    e = x+y+z
+    assert len(e.args) == 3
+
+def test_args():
+    assert (x*y).args[:] in ((x, y), (y, x))
+    assert (x+y).args[:] in ((x, y), (y, x))
+    assert (x*y+1).args[:] in ((x*y, 1), (1, x*y))
+    #assert sin(x*y).args[:] == (x*y,)
+    #assert sin(x*y).args[0] == x*y
+    assert (x**y).args[:] == (x,y)
+    assert (x**y).args[0] == x
+    assert (x**y).args[1] == y
