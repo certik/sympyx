75cbb80a1e76058690e2651bb03eb271858d13e4
Ondrej Certik <ondrej@certik.cz>
2008-08-12 22:25:29 +0200

    benchmarks added
    Signed-off-by: Kirill Smelkov <kirr@landau.phys.spbu.ru>

---
+++ benchmarks.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from timeit import default_timer as clock
+try:
+    from sympy import Symbol
+except:
+    from sympycore import Symbol
+
+x = Symbol("x")
+y = Symbol("y")
+z = Symbol("z")
+
+def bench1():
+    "e=(x+y+z+1)**10;f=e*(e+1);f.expand()"
+    e = (x+y+z+1)**10
+    f = e*(e+1)
+    f = f.expand()
+
+def bench2():
+    "e=(x+y+z+1)**10; f=e**2+e; f.expand()"
+    e = (x+y+z+1)**10
+    f = e**2 + e
+    f = f.expand()
+
+def bench3():
+    "e=(x+y+z+1)**50; e.expand()"
+    e = (x+y+z+1)**50
+    e = e.expand()
+
+benchmarks = [
+        bench1,
+        bench2,
+        bench3,
+        ]
+
+report = []
+for b in benchmarks:
+    t = clock()
+    b()
+    t = clock()-t
+    print "%65s: %f" % (b.__doc__, t)
