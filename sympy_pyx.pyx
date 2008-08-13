5f996fe5ee5d7823a3194d352803d56f300197a2
Ondrej Certik <ondrej@certik.cz>
2008-08-13 23:15:03 +0200

    Move var to sympy.py file, it starts working then.
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>

--- sympy_pyx.pyx
+++ sympy_pyx.pyx
@@ -565,61 +565,6 @@ cpdef Basic sympify(x):
         return Integer(x)
     return x
 
-def var(s):
-    """
-        Create a symbolic variable with the name *s*.
-
-        INPUT:
-            s -- a string, either a single variable name, or
-                 a space separated list of variable names, or
-                 a list of variable names.
-
-        NOTE: The new variable is both returned and automatically injected into
-        the parent's *global* namespace.  It's recommended not to use "var" in
-        library code, it is better to use symbols() instead.
-
-        EXAMPLES:
-        We define some symbolic variables:
-            >>> var('m')
-            m
-            >>> var('n xx yy zz')
-            (n, xx, yy, zz)
-            >>> n
-            n
-
-    """
-    import re
-    import inspect
-    frame = inspect.currentframe().f_back
-
-    try:
-        if not isinstance(s, list):
-            s = re.split('\s|,', s)
-
-        res = []
-
-        for t in s:
-            # skip empty strings
-            if not t:
-                continue
-            sym = _Symbol(t)
-            frame.f_globals[t] = sym
-            res.append(sym)
-
-        res = tuple(res)
-        if len(res) == 0:   # var('')
-            res = None
-        elif len(res) == 1: # var('x')
-            res = res[0]
-                            # otherwise var('a b ...')
-        return res
-
-    finally:
-        # we should explicitly break cyclic dependencies as stated in inspect
-        # doc
-        #del frame
-        pass
-
 def binomial_coefficients(n):
     """Return a dictionary containing pairs {(k1,k2) : C_kn} where
     C_kn are binomial coefficients and n=k1+k2."""
